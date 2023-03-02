b754d27ffec23c1892d74551ec96d8901a0f06c3
Robert <seoinsighters@gmail.com>
2023-03-02 21:01:24 +0000

    Export Languages component

---
+++ packages/apps/lang-translator/src/App.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+const App = (): JSX.Element => {
+    return (
+        <div>
+            <h3>Hello World</h3>
+        </div>
+    )
+};
+
+export default App;
---
+++ packages/apps/lang-translator/src/index.ts
@@ -0,0 +1,5 @@
+import("./App.js").then(() => {
+
+});
+
+export default 0;
